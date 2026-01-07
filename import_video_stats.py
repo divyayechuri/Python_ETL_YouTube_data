1568d0b2e0edf793bdaa11368116e2ec2a6f1900
DivyaYechuri <yechuri_divya@yahoo.com>
2026-01-07 16:28:14 -0600

    Wrote the python script to get the Channel Paylist ID from the Youtube channel. Added .env file to create environent veriables to protect sensitive paramaters. For example, here the API_KEY is sensitive. Finally added the .env to .gitignore because we don't want to push it to our public github repo. It will defeat the purpose of having API_KEY in the .env file.

---
+++ import_video_stats.py
@@ -0,0 +1,3 @@
+import video_stats
+
+print("Running script from import_video_stats.py")
---
+++ video_stats.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+import os 
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv("API_KEY")
+CHANNEL_HANDLE = "MrBeast"
+
+def get_playlist_id():
+    try:    
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        #print(response)
+        #print(response.text)
+
+        data = response.json()
+
+        # print("PRINTING DATA:")
+        # print(data)
+
+        # print("PRINTING JSON:")
+        # print(json.dumps(data, indent=4))
+
+        # print("PRINTING ONE ITEM:")
+
+        channel_items = data["items"][0]
+
+        channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+
+        print(channel_playlist_id)
+        return channel_playlist_id
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__":
+    print("get_playlist_id will be executed")
+    get_playlist_id()
+else:
+    print("get_playlist_id won't be executed")
