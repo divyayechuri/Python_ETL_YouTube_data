38e15127fb0a853a176847ffeead6149e6bd7484
DivyaYechuri <yechuri_divya@yahoo.com>
2026-01-14 13:53:35 -0600

    Added python files for Inserts, Updates and Deletes.

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,17 +1,21 @@
 import requests
 import json
 
-import os 
-from dotenv import load_dotenv
+# import os 
+# from dotenv import load_dotenv
 
 from datetime import date
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+# load_dotenv(dotenv_path="./.env")
+
+API_KEY = Variable.get("API_KEY") #os.getenv("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE") #os.getenv("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
     try:    
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
@@ -44,7 +48,7 @@ def get_playlist_id():
         raise e
     
 # playlist_id = get_playlist_id() -- this will be later moved to the main function
-
+@task
 def get_video_ids(playlist_id):
     
     video_ids = []
@@ -83,7 +87,7 @@ def get_video_ids(playlist_id):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -127,7 +131,7 @@ def extract_video_data(video_ids):
 
     except requests.exceptions.RequestException as e:
         raise e
-    
+@task    
 def save_to_json(extracted_data):
     file_path = f"./data/YouTube_data_{date.today()}.json"
 
